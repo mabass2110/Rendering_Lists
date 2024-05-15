7684d863fbbd64eb48b5c09290a0c0c9efedc587
helder mabasso <hfmabasso@gmail.com>
2024-05-15 14:40:59 +0200

    Starter Files

---
+++ src/navBarNoevensEx01.jsx
@@ -0,0 +1,21 @@
+// Ex 1 - remove all even numbers from the NavBar
+// then make a Button for each instead of an <li>
+function NavBar({ menuitems }) {
+  const { Button } = ReactBootstrap;
+  
+  const oddList = menuitems.filter(item=>{
+    item % 2 === 1
+  })
+  
+  const updatedList = oddList.map((listItems, index) => {
+    return <Button key={index.toString()}>{listItems}</Button>;
+  });
+
+  // note that React needs to have a single Parent
+  return <ul style={{ listStyleType: "none" }}>{updatedList}</ul>;
+}
+const menuItems = [1, 2, 3, 4, 5];
+ReactDOM.render(
+  <NavBar menuitems={menuItems} />,
+  document.getElementById("root")
+);
