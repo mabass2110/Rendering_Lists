c74f265c3dfe0beb459b9434f7c85c1b735f0690
mabass2110 <131558331+mabass2110@users.noreply.github.com>
2024-05-17 15:57:45 +0200

    Update navBarSaveClicks.jsx
    Commenting the code.

--- src/navBarSaveClicks.jsx
+++ src/navBarSaveClicks.jsx
@@ -1,15 +1,25 @@
 // use of "props" to set data
 function NavBar(props) {
+  //clicks state
   const [clicks, setClicks] = React.useState([]);
   console.log(`clicks: ${clicks}`);
   alert(`Rendering NavBar`);
+
+  
   const list = props.menuitems;
+
+  //Destructuring Button from ReactBootstrap
   const { Button } = ReactBootstrap;
+
+
+  //onClick handler
   // each item should have an unique key
-  const handlßeClick = e => {
+  const handleClick = e => {
     console.log(`clicked on: ${e.target.innerHTML}`);
     setClicks([...clicks, e.target.innerHTML]);
   };
+
+  //latest version of the list
   const updatedList = list.map((listItems, index) => {
     return (
       <Button onClick={handleClick} key={index}>
@@ -17,9 +27,11 @@ function NavBar(props) {
       </Button>
     );
   });
+  
   // note that React needs to have a single Parent
   return <ul>{updatedList}</ul>;
 }
+//array that will be passed as a value of menuItems props
 const menuItems = [1, 2, 3, 4, 5];
 ReactDOM.render(
   <NavBar menuitems={menuItems} />,
